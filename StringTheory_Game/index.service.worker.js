1757415f29cc36c99b05c4a0bd1fc89df4b7f99f
AndrewViesta <drewviesta@yahoo.com>
2023-09-19 23:06:03 -0400

    On screen keyboard fix?

--- StringTheory_Game/index.service.worker.js
+++ StringTheory_Game/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1695177624|1040500341";
+const CACHE_VERSION = "1695179135|2551279843";
 const CACHE_PREFIX = "String Theory-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
